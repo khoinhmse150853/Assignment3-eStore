92099b7ff56e1d34e5df1ef69b88b98742703c17
Admin <Admin@DESKTOP-9M77UUJ>
2022-07-20 12:33:52 +0700

    Pham Hoang Nam - chuc nang Member

---
+++ DataAccess/Repository/IMemberRepository.cs
@@ -0,0 +1,19 @@
+﻿using BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public interface IMemberRepository
+    {
+        IEnumerable<TblMember> GetMembersList(string cs);   
+        TblMember GetMembersById(string cs, int Id);
+        IEnumerable<TblMember> SearchMembers(string cs, int id);
+        void Update(string cs, TblMember member);
+        void AddNew(string cs, TblMember member);
+        void Remove(string cs, int memberId);
+    }
+}
